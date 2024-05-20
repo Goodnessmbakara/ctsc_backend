d8967363b327e32c3b442f96e7312642ad2a9fae
Goodnessmbakara <amicablembakara50@gmail.com>
2024-05-20 17:06:23 +0100

    taking out daphne

--- ctsc_backend/settings.py
+++ ctsc_backend/settings.py
@@ -24,7 +24,7 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 INSTALLED_APPS = [
-    "daphne",
+    #"daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
