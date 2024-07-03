95fe96ed4c8b8fa759cdad6976abd58c596d0880
Goodnessmbakara <amicablembakara50@gmail.com>
2024-07-03 15:32:48 +0100

    fixed the partner pics ish

--- src/serializers.py
+++ src/serializers.py
@@ -12,11 +12,11 @@ class TeamMemberSerializer(serializers.ModelSerializer):
         model = TeamMember
         fields = '__all__'
 class PartnerSerializer(serializers.ModelSerializer):
-    partner_pics = serializers.SerializerMethodField()
+    # partner_pics = serializers.SerializerMethodField()
 
-    def get_partner_pics(self, obj):
-        if obj.partner_pics:
-            return self.context['request'].build_absolute_uri(obj.partner_pics.url)
+    # def get_partner_pics(self, obj):
+    #     if obj.partner_pics:
+    #         return self.context['request'].build_absolute_uri(obj.partner_pics.url)
     class Meta:
         model = Partner
         fields = ('id', 'partner_pics', 'description')
